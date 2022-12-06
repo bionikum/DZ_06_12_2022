739ebc5295564fa19ad221c0684dd8d1f882a78f
bionikum <bionikum@yandex.ru>
2022-12-06 18:18:21 +0500

    Task 19

---
+++ 19_task/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 100000 && n > 10000 || n > -10000 && n < -10000)
+{
+    if (n % 10 == n / 10000 && (n % 100 - n % 10) / 10 == n / 1000 % 10)
+
+        Console.Write("Число является палиндромом.");
+
+    else
+
+        Console.Write("Число не является палиндромом.");
+}
+
+else
+
+    Console.WriteLine("Вы ввели не пятизначное число..");
