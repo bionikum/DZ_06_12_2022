d4b417dd5a7e29bb1621bd1bf7826e7d00d64e20
bionikum <bionikum@yandex.ru>
2022-12-06 19:14:42 +0500

    Task 19, добавил минусовые значения

--- 19_task/Program.cs
+++ 19_task/Program.cs
@@ -1,10 +1,15 @@
 ﻿Console.Clear();
 Console.Write("Введите пятизначное число : ");
 int n = Convert.ToInt32(Console.ReadLine());
+int m = n * (-1);
 
-if (n < 100000 && n > 9999)
+if (n < 100000 && n > 9999 || m < 100000 && m > 9999)
 {
     if (n % 10 == n / 10000 && (n % 100 - n % 10) / 10 == n / 1000 % 10)
+
+        Console.Write("Число является палиндромом.");
+
+    else if (m % 10 == m / 10000 && (m % 100 - m % 10) / 10 == m / 1000 % 10)
 
         Console.Write("Число является палиндромом.");
 
