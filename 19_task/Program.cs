d8dbcc601aab00182709ea7ee14a4a5ede14cdcb
bionikum <bionikum@yandex.ru>
2022-12-07 18:47:31 +0500

    Task 19, использовал строку

--- 19_task/Program.cs
+++ 19_task/Program.cs
@@ -1,19 +1,18 @@
 ﻿Console.Clear();
 Console.Write("Введите пятизначное число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int m = Math.Abs(n);
-
-if (m < 100000 && m > 9999)
-{
-    if (m % 10 == m / 10000 && (m % 100 - m % 10) / 10 == m / 1000 % 10)
-
-        Console.Write("Число является палиндромом.");
-    else
-
-        Console.Write("Число не является палиндромом.");
-}
-
-else
-
-    Console.WriteLine("Вы ввели не пятизначное число!");
-
+            string name = Console.ReadLine();
+            string reverse = string.Empty;
+            
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reverse += name[i];
+            }
+            
+            if (name == reverse)
+            {
+                Console.WriteLine($"{name} является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine($"{name} не является палиндромом");
+            }
